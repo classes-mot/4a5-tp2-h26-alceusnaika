32c23032ae02c562db9cc7008f56db2c8f71c66c
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-03-29 13:37:44 -0400

    FAIT : games.js

---
+++ backend/models/games.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const gameSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    categorie: { type: String, required: true },
+    description: { type: String },
+    nbJoueurs: { type: Number, required: true },
+    dureePartie: { type: Number, required: true },
+
+});
+
+export const Game = mongoose.model('Game', gameSchema);
