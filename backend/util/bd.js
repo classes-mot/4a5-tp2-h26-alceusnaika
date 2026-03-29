929cd56e3db4dd98c0c16621d976333e130bb317
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-03-29 13:55:22 -0400

    FAIT : bd.js

---
+++ backend/util/bd.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectDB = async () => {
+    if (isConnected) return;
+
+    let uri = 'mongodb://localhost:27017/tp24A5';
+
+    try {
+        await mongoose.connect(uri);
+        isConnected = true;
+        console.log("Connectée à la BD avec succès !");
+    } catch (err) {
+        console.log('Erreur lors de la connexion', err);
+        process.exit(1);
+    }
+}
