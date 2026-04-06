a67d36a393d6c87527ef5210afa17fb636aa9d5b
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-04-06 12:48:07 -0400

    FAIT: correction game.js

--- backend/models/games.js
+++ backend/models/games.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const gameSchema = new mongoose.Schema({
+    id: { type: Number, required: true },
     title: { type: String, required: true },
     categorie: { type: String, required: true },
     description: { type: String },
