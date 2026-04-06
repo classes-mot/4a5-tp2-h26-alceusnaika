5c44b07db870142554ee8a8eb266cedab57c0440
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-04-06 14:52:25 -0400

    FAIT: users-routes.js

---
+++ backend/routes/users-routes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import usersController from '../controllers/users-controllers.js';
+const router = express.Router();
+
+router.get('/', usersController.getUsers);
+
+router.get('/profile/:uid', usersController.getUserById);
+
+router.post('/reguster', usersController.registerUser);
+
+router.post('login', usersController.login);
+
+router.patch('profile:/uid', usersController.updatedUserById);
+
+export default router;
