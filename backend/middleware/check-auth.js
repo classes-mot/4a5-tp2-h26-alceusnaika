01e756cfe64519eaa4d8197301938495a6fb04d1
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-04-08 13:37:41 -0400

    FAIT: ajout d'une vérification du type de méthode et affichage de userData dans la console - check-auth.js

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -3,12 +3,16 @@ import HttpError from "../util/http-error.js";
 
 const checkAuth = (req, res, next) => {
     try {
+        if (req.method === 'OPTIONS') {
+            return next();
+        }
         const token = req.headers.authorization.split('')[1];
         if (!token) {
             throw new Error('Authentification échouée !');
         }
         const decodedToken = jwt.verify(token, 'cleSecrete!');
         req.userData = { userId: decodedToken.userId };
+        console.log(req.userData);
         next();
     } catch (err) {
         const error = new HttpError('Authentification failed!', 401);
